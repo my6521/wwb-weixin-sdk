3a2d2fa5fb2dd265bc8bed3081bfbb57c6b58896
wwb <22379099@qq.com>
2024-03-28 11:35:47 +0800

    优化

--- src/WWB.Weixin.Work/ITokenManager.cs
+++ src/WWB.Weixin.Work/ITokenManager.cs
@@ -5,12 +5,29 @@ using System.Threading.Tasks;
 
 namespace WWB.Weixin.Work
 {
+    /// <summary>
+    /// 凭据管理
+    /// </summary>
     public interface ITokenManager
     {
+        /// <summary>
+        /// 获取服务商凭据
+        /// </summary>
+        /// <returns></returns>
         Task<string> GetProviderToken();
 
+        /// <summary>
+        /// 获取应用凭据
+        /// </summary>
+        /// <returns></returns>
         Task<string> GetSuiteToken();
 
+        /// <summary>
+        /// 获取企业授权凭据
+        /// </summary>
+        /// <param name="corpId"></param>
+        /// <param name="corpsecret"></param>
+        /// <returns></returns>
         Task<string> GetToken(string corpId, string corpsecret);
     }
 }
