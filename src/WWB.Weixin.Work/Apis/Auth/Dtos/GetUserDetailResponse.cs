a468f14fb7ed490d54e1789616d960afe9701123
wwb <22379099@qq.com>
2024-05-19 12:15:17 +0800

    1

--- src/WWB.Weixin.Work/Apis/Auth/Dtos/GetUserDetailResponse.cs
+++ src/WWB.Weixin.Work/Apis/Auth/Dtos/GetUserDetailResponse.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace WWB.Weixin.Work.Apis.Auth.Dtos
 {
     public class GetUserDetailResponse : ApiResultBase
     {
+        [JsonProperty("userid")]
+        public string UserId { get; set; }
+
+        [JsonProperty("gender")]
+        public int Gender { get; set; }
+
+        [JsonProperty("avatar")]
+        public string Avatar { get; set; }
+
+        [JsonProperty("mobile")]
+        public string Mobile { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("biz_mail")]
+        public string BizMail { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
     }
 }
