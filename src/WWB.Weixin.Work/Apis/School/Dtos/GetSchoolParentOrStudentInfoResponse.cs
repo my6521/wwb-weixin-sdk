3b53cb69e331945d1421ca1fb87557a92732f00b
wwb <my6521@qq.com>
2024-08-27 23:50:09 +0800

    1.0.4

--- src/WWB.Weixin.Work/Apis/School/Dtos/GetSchoolParentOrStudentInfoResponse.cs
+++ src/WWB.Weixin.Work/Apis/School/Dtos/GetSchoolParentOrStudentInfoResponse.cs
@@ -2,9 +2,19 @@
 {
     public class GetSchoolParentOrStudentInfoResponse : ApiResultBase
     {
+        /// <summary>
+        /// 用户类型:1表示学生，2表示家长
+        /// </summary>
         public int user_type { get; set; }
 
+        /// <summary>
+        /// 学生信息
+        /// </summary>
         public SchoolStudentInfo student { get; set; }
+
+        /// <summary>
+        /// 家长信息
+        /// </summary>
         public SchoolParentInfo parent { get; set; }
     }
 }
